8bb82bbed44b94bbaf6393648467fcc1d00b255f
luochunhua <luochunhua1996@outlook.com>
2022-05-08 01:02:02 +0800

    [Enhance] Support verification code decoding

--- clock-in.py
+++ clock-in.py
@@ -2,44 +2,48 @@
 
 # 打卡脚修改自ZJU-nCov-Hitcarder的开源代码，感谢这位同学开源的代码
 
-import requests
+import datetime
 import json
 import re
-import datetime
-import time
 import sys
+import time
+
+from ddddocr import DdddOcr
+import requests
 
 
-class ClockIn(object):
+class DaKa(object):
     """Hit card class
-
     Attributes:
         username: (str) 浙大统一认证平台用户名（一般为学号）
         password: (str) 浙大统一认证平台密码
-        LOGIN_URL: (str) 登录url
-        BASE_URL: (str) 打卡首页url
-        SAVE_URL: (str) 提交打卡url
-        HEADERS: (dir) 请求头
+        login_url: (str) 登录url
+        base_url: (str) 打卡首页url
+        save_url: (str) 提交打卡url
+        self.headers: (dir) 请求头
         sess: (requests.Session) 统一的session
     """
-    LOGIN_URL = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
-    BASE_URL = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
-    SAVE_URL = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
-    HEADERS = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
-    }
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        self.imgaddress = 'https://healthreport.zju.edu.cn/ncov/wap/default/code'
+        self.login_url = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
+        self.base_url = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
+        self.save_url = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
+        self.headers = {
+            "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+        }
         self.sess = requests.Session()
+        self.ocr_classifier = DdddOcr(show_ad=False, old=True)
 
     def login(self):
         """Login to ZJU platform"""
-        res = self.sess.get(self.LOGIN_URL, headers=self.HEADERS)
-        execution = re.search(
-            'name="execution" value="(.*?)"', res.text).group(1)
-        res = self.sess.get(
-            url='https://zjuam.zju.edu.cn/cas/v2/getPubKey', headers=self.HEADERS).json()
+        res = self.sess.get(self.login_url, headers=self.headers)
+        execution = re.search('name="execution" value="(.*?)"',
+                              res.text).group(1)
+        res = self.sess.get(url='https://zjuam.zju.edu.cn/cas/v2/getPubKey',
+                            headers=self.headers).json()
         n, e = res['modulus'], res['exponent']
         encrypt_password = self._rsa_encrypt(self.password, e, n)
 
@@ -49,7 +53,9 @@ class ClockIn(object):
             'execution': execution,
             '_eventId': 'submit'
         }
-        res = self.sess.post(url=self.LOGIN_URL, data=data, headers=self.HEADERS)
+        res = self.sess.post(url=self.login_url,
+                             data=data,
+                             headers=self.headers)
 
         # check if login successfully
         if '统一身份认证' in res.content.decode():
@@ -58,7 +64,9 @@ class ClockIn(object):
 
     def post(self):
         """Post the hitcard info"""
-        res = self.sess.post(self.SAVE_URL, data=self.info, headers=self.HEADERS)
+        res = self.sess.post(self.save_url,
+                             data=self.info,
+                             headers=self.headers)
         return json.loads(res.text)
 
     def get_date(self):
@@ -69,7 +77,7 @@ class ClockIn(object):
     def get_info(self, html=None):
         """Get hitcard info, which is the old info with updated new time."""
         if not html:
-            res = self.sess.get(self.BASE_URL, headers=self.HEADERS)
+            res = self.sess.get(self.base_url, headers=self.headers)
             html = res.content.decode()
 
         try:
@@ -83,6 +91,17 @@ class ClockIn(object):
             new_id = new_info_tmp['id']
             name = re.findall(r'realname: "([^\"]+)",', html)[0]
             number = re.findall(r"number: '([^\']+)',", html)[0]
+            try:
+                res = self.sess.get(self.imgaddress, headers=self.headers)
+                code = self.ocr_classifier.classification(res.content)
+                if not code:
+                    self.Push('验证码识别失败，请重试')
+                    return
+                else:
+                    self.Push('验证码识别成功，请稍后')
+            except:
+                print('验证码识别失败')
+
         except IndexError:
             raise RegexMatchError('Relative info not found in html with regex')
         except json.decoder.JSONDecodeError:
@@ -101,18 +120,13 @@ class ClockIn(object):
         # form change
         new_info['jrdqtlqk[]'] = 0
         new_info['jrdqjcqk[]'] = 0
-        new_info['sfsqhzjkk'] = 1   # 是否申领杭州健康码
-        new_info['sqhzjkkys'] = 1   # 杭州健康吗颜色，1:绿色 2:红色 3:黄色
-        new_info['sfqrxxss'] = 1    # 是否确认信息属实
+        new_info['sfsqhzjkk'] = 1  # 是否申领杭州健康码
+        new_info['sqhzjkkys'] = 1  # 杭州健康吗颜色，1:绿色 2:红色 3:黄色
+        new_info['sfqrxxss'] = 1  # 是否确认信息属实
         new_info['jcqzrq'] = ""
         new_info['gwszdd'] = ""
         new_info['szgjcs'] = ""
-
-        # 2021.08.05 Fix 2
-        magics = re.findall(r'"([0-9a-f]{32})":\s*"([^\"]+)"', html)
-        for item in magics:
-            new_info[item[0]] = item[1]
-
+        new_info['verifyCode'] = code
         self.info = new_info
         return new_info
 
@@ -143,7 +157,6 @@ class DecodeError(Exception):
 
 def main(username, password):
     """Hit card process
-
     Arguments:
         username: (str) 浙大统一认证平台用户名（一般为学号）
         password: (str) 浙大统一认证平台密码
@@ -152,7 +165,7 @@ def main(username, password):
           datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     print("🚌 打卡任务启动")
 
-    dk = ClockIn(username, password)
+    dk = DaKa(username, password)
 
     print("登录到浙大统一身份认证平台...")
     try:
@@ -170,17 +183,13 @@ def main(username, password):
         print('获取信息失败，请手动打卡，更多信息: ' + str(err))
         raise Exception
 
-    print('正在为您打卡')
+    print('正在为您打卡打卡打卡')
     try:
         res = dk.post()
         if str(res['e']) == '0':
             print('已为您打卡成功！')
         else:
             print(res['m'])
-            if res['m'].find("已经") != -1: # 已经填报过了 不报错
-                pass
-            else:
-                raise Exception
     except Exception:
         print('数据提交失败')
         raise Exception
